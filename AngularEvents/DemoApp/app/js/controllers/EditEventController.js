2f3bdc4fcfaff05616d5757f863111101dd35e45
Jeff <jeffreylongo@yahoo.com>
2018-01-24 07:59:26 -0500

    finished angular controlllers and markup, next is Services

--- AngularEvents/DemoApp/app/js/controllers/EditEventController.js
+++ AngularEvents/DemoApp/app/js/controllers/EditEventController.js
@@ -2,6 +2,15 @@
 
 eventsApp.controller('EditEventController',
     function EditEventController($scope) {
+        $scope.saveEvent = function (event, newEventForm) {
+            console.log(newEventForm);
+            if (newEventForm.$valid) {
+                window.alert('event ' + event.name + ' saved!');
+            }
+        };
 
+        $scope.cancelEdit = function () {
+            window.location = "/EventDetails.html";
+        }
     }
 );
